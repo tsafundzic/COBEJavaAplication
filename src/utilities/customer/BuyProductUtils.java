4241e5ce71853902cac586ef07967d95fa30a2de
tsafundzic <tsafundzic@gmail.com>
2018-03-15 13:56:11 +0100

    Implemented ProductAmount

--- src/utilities/customer/BuyProductUtils.java
+++ src/utilities/customer/BuyProductUtils.java
@@ -2,14 +2,22 @@ package utilities.customer;
 
 import model.Customer;
 import model.Product;
+import model.ProductAmount;
+import utilities.PrintUtils;
+import utilities.menu.MenuSelectorUtils;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class BuyProductUtils {
+
+    static List<ProductAmount> productAmounts = new ArrayList<>();
+
     public static void buyProduct(Product product, Customer customer, List<Customer> customers, List<Product> products) {
         Scanner scanner = new Scanner(System.in);
-        int inputvalue;
+        int inputAmount;
+        int inputValue;
 
         System.out.println("Unesi količinu proizvoda:");
 
@@ -17,20 +25,33 @@ public class BuyProductUtils {
             System.out.println("Pogrešan unos!");
             scanner.next();
         }
-        inputvalue = scanner.nextInt();
+        inputAmount = scanner.nextInt();
 
         double customerBalance = customer.getBalance();
-        double productPrice = product.getProductPrice() * inputvalue;
-        double newBalance = customerBalance - productPrice;
 
-        if (newBalance >= 0) {
-            customer.setBalance(newBalance);
-            System.out.println(product.getProductName() + " " + product.getProductID() + customer.getName());
-            System.out.println(" \nKupnja uspješno obavljena! \n \t" + product.getProductName() + " \t" + product.getProductPrice() + "\t" + inputvalue + "\t" + productPrice + "\n\tNovo stanje na računu: " + customer.getBalance() + " \n");
-            CustomerFirstChooserUtils.customerFirstChooser(customers, customer, products);
-        } else {
-            System.out.println("Nedovoljno sredstava na računu!\n");
-            CustomerFirstChooserUtils.customerFirstChooser(customers, customer, products);
+        productAmounts.add(new ProductAmount(product.getProductID(), product.getProductName(), product.getProductPrice(), inputAmount));
+
+        System.out.println("Odaberi: \n 1. Nastavi kupnju \n 2. Završi kupnju");
+        inputValue = MenuSelectorUtils.menuSelector(2);
+
+        if (inputValue == 1) {
+            PrintUtils.showAvailableProducts(customers, products, customer);
+        } else if (inputValue == 2) {
+            customer.setBalance(customerBalance);
+            for (ProductAmount productAmount : productAmounts) {
+                System.out.println(productAmount.getProductName() + "\t" + productAmount.getAmount() + "\t" + productAmount.getAmount() * productAmount.getProductPrice());
+                double productPrice = productAmount.getProductPrice() * productAmount.getAmount();
+                customerBalance = customerBalance - productPrice;
+            }
+            if (customerBalance >= 0) {
+                System.out.println("Novo stanje: " + customerBalance);
+                customer.setBalance(customerBalance);
+                productAmounts.clear();
+                CustomerFirstChooserUtils.customerFirstChooser(customers, customer, products);
+            } else {
+                System.out.println("Nedovoljno sredstava na računu!\n");
+                CustomerFirstChooserUtils.customerFirstChooser(customers, customer, products);
+            }
         }
     }
 }
